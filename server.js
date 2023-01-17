44336fad0f1a71886d3d9e662122576ddf4f5433
MONE'M <60390230+Ahmed1monm@users.noreply.github.com>
2023-01-17 02:56:40 +0200

    connect database

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const morgan = require('morgan');
-dotenv.config({path: 'config.env'});
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({path:'config.env'});
+
+
 
 const app = express();
+
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING).then((conn)=>{
+    console.log(`Database connected at ${conn.connection.host}`);
+}).catch((err)=>{
+    console.log(`database error ${err}`);
+});
 
 if(process.env.MODE_ENV == "development"){
     app.use(morgan('dev'));
     console.log(`mode: ${ process.env.MODE_ENV }`);
+    // process.exit(1);
 }
 
 app.get("/",(req,res)=>{
     res.send("Hello, World");
 })
 
-const PORT = process.env.PORT;
+ const PORT = process.env.PORT;
 app.listen(PORT,()=>{
     console.log("PORT " + PORT);   
 })
