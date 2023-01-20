b858fa6bef4be58393d68f11776147e488c19c5e
MONE'M <60390230+Ahmed1monm@users.noreply.github.com>
2023-01-20 21:54:07 +0200

    catergory schema

--- models/category.js
+++ models/category.js
@@ -3,8 +3,25 @@ const mongoose = require('mongoose');
 
 // Create schema
 const CategorySchema = new mongoose.Schema({
-    name: String,
-});
+    name: {
+        type: String,
+        minLength: [3,"Category name is too short"],
+        minLength: [32,"Category name is too Long"],
+
+        unique: [true, "Category name must be unique"],
+        required: [true, "Category name is required"]
+
+    },
+    slug: {
+        type: String,
+        lowerCase: true
+
+    },
+},
+{
+    timestamps: true
+}
+);
 
 //Create model
 const CategoryModel = new mongoose.model('Categort', CategorySchema);
