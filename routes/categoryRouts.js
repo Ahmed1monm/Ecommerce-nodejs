ed03e92186329ce1f0cd721ae309db0ac4811690
MONE'M <60390230+Ahmed1monm@users.noreply.github.com>
2023-01-22 05:25:26 +0200

    CRUD category validations

--- routes/categoryRouts.js
+++ routes/categoryRouts.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const categoryController = require('../services/categoryController');
-const{ getCategoryValidator} = require('../utils/validators/categoryValidator');
+const{ getCategoryValidator , updateCategoryValidator, deleteCategoryValidator, createCategoryValidator} = require('../utils/validators/categoryValidator');
 const router = express.Router();
 
-router.route("/").post(categoryController.createCategory).get(categoryController.getCategories);
+router.route("/")
+    .post(createCategoryValidator, categoryController.createCategory)
+    .get(categoryController.getCategories);
 router.route('/:id')
     .get(getCategoryValidator, categoryController.getCategoryByID)
-    .put(categoryController.updateCategory)
-    .delete(categoryController.deleteCategoryByID);
+    .put(updateCategoryValidator, categoryController.updateCategory)
+    .delete(deleteCategoryValidator, categoryController.deleteCategoryByID);
 
 module.exports = router;
--- utils/validators/categoryValidator.js
+++ utils/validators/categoryValidator.js
@@ -4,5 +4,27 @@ const validationMiddelware = require('../../middlewares/validator');
 exports.getCategoryValidator = [
     check('id').isMongoId().withMessage("Invalid mongo ID format"), // Rule
     validationMiddelware // Validator Middelware
+]
 
+exports.createCategoryValidator =[
+    check('name')
+    .notEmpty()
+    .withMessage("Category name is required")
+    .isLength({min:3})
+    .withMessage("Category name is too short")
+    .isLength({max: 32})
+    .withMessage("Category name is too Long"),
+    validationMiddelware
+]
+
+
+exports.updateCategoryValidator = [
+    check('id').isMongoId().withMessage("Invalid mongo ID format"), // Rule
+    validationMiddelware // Validator Middelware
+]
+
+
+exports.deleteCategoryValidator = [
+    check('id').isMongoId().withMessage("Invalid mongo ID format"), // Rule
+    validationMiddelware // Validator Middelware
 ]
